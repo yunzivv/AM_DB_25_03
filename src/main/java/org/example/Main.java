4656dcf7095aff0579ddb8f12882fa3c79e64ed0
yunzivv <yunzivv@gmail.com>
2025-03-28 17:24:29 +0900

    수정, 삭제 시 해당 article 존재 여부 확인 기능 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -36,9 +36,7 @@ public class Main {
                 PreparedStatement pstmt = null;
 
                 try {
-                    // 드라이버 연결
                     Class.forName("org.mariadb.jdbc.Driver");
-                    // 계정 연결
                     String url = "jdbc:mariadb://127.0.0.1:3306/AM_DB_25_03?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
 
                     conn = DriverManager.getConnection(url, "root", "");
@@ -50,11 +48,9 @@ public class Main {
                     sql += "title = '" + title + "', ";
                     sql += "`body` = '" + body + "';";
 
-                    // sql 명령어를 전달한다.
                     pstmt = conn.prepareStatement(sql);
                     System.out.println(sql);
 
-                    // 몇개의 행에 적용 되었는 지 반환
                     int affectedrows = pstmt.executeUpdate();
                     System.out.println("sql affecte rows : " + affectedrows);
 
@@ -82,9 +78,7 @@ public class Main {
                 PreparedStatement pstmt = null;
 
                 try {
-                    // 드라이버 연결
                     Class.forName("org.mariadb.jdbc.Driver");
-                    // 계정 연결
                     String url = "jdbc:mariadb://127.0.0.1:3306/AM_DB_25_03?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
 
                     conn = DriverManager.getConnection(url, "root", "");
@@ -112,7 +106,6 @@ public class Main {
                         articles.add(article);
                     }
 
-                    // article 객체를 저장한 articles 출력
                     System.out.println(" No |  title  |  regDate  ");
                     System.out.println("--------------------");
                     for (Article article : articles) {
@@ -128,15 +121,15 @@ public class Main {
                         System.out.println(" " + id + " | " + title + " | " + regDate);
                     }
 
-                    // 몇개의 행에 적용 되었는 지 반환
-                    // select 실행은 적용 행이 없음
                     int affectedrows = pstmt.executeUpdate();
                     System.out.println("rows : " + affectedrows);
 
                 } catch (ClassNotFoundException e) {
                     System.out.println("드라이버 로딩 실패" + e);
+
                 } catch (SQLException e) {
                     System.out.println("에러 : " + e);
+
                 } finally {
                     try {
                         if (conn != null && !conn.isClosed()) {
@@ -147,23 +140,42 @@ public class Main {
                     }
                 }
 
-            } else if (cmd.equals("article modify")) {
+            } else if (cmd.startsWith("article modify")) {
+
+                int modifyId = -1;
+
+                try {
+                    modifyId = Integer.parseInt(cmd.split(" ")[2]);
+                } catch (Exception e) {
+                    System.out.println("Enter aritlce Number");
+                }
 
                 Connection conn = null;
                 PreparedStatement pstmt = null;
 
                 try {
-                    // 드라이버 연결
                     Class.forName("org.mariadb.jdbc.Driver");
-                    // 계정 연결
                     String url = "jdbc:mariadb://127.0.0.1:3306/AM_DB_25_03?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
 
                     conn = DriverManager.getConnection(url, "root", "");
                     System.out.println("연결 성공!");
 
-                    System.out.print("\n수정할 게시물 번호 : ");
-                    int modifyNum = sc.nextInt();
-                    sc.nextLine();
+                    String sql = "select count(*) from article where id = " + modifyId + ";";
+                    System.out.println(sql);
+                    pstmt = conn.prepareStatement(sql);
+                    ResultSet rs = pstmt.executeQuery();
+
+                    int modifyIdCount = -1;
+
+                    while(rs.next()) {
+                        modifyIdCount = rs.getInt("count(*)");
+                    }
+
+                    if (modifyIdCount == 0) {
+                        System.out.printf("article %d is not exist\n", modifyId);
+                        continue;
+                    }
+
 
                     System.out.print("new Title : ");
                     String title = sc.nextLine().trim();
@@ -171,7 +183,7 @@ public class Main {
                     System.out.print("new Body : ");
                     String body = sc.nextLine().trim();
 
-                    String sql = "update article";
+                    sql = "update article";
                     sql += " set updateDate = now()";
                     if (title.length() > 0) {
                         sql += ", title = '" + title + "'";
@@ -179,28 +191,21 @@ public class Main {
                     if (body.length() > 0) {
                         sql += ", `body` = '" + body + "'";
                     }
-                    sql += " where id = " + modifyNum;
+                    sql += " where id = " + modifyId;
 
-                    // sql 명령어를 전달한다.
                     pstmt = conn.prepareStatement(sql);
-                    System.out.println(sql);
 
-                    // sql 명령어 전달 결과를 저장
-                    ResultSet rs = pstmt.executeQuery();
+                    rs = pstmt.executeQuery();
 
-                    // rs에 결과를 저장해도 자바에서는 결과값을 해석할 수 없다.
-                    // 자바가 읽을 수 있는 자료로 바꿔준다.
-                    List<Article> articles = new ArrayList<>();
-
-
-                    // 몇개의 행에 적용 되었는 지 반환
                     int affectedrows = pstmt.executeUpdate();
                     System.out.println("rows : " + affectedrows);
 
                 } catch (ClassNotFoundException e) {
                     System.out.println("드라이버 로딩 실패" + e);
+
                 } catch (SQLException e) {
                     System.out.println("에러 : " + e);
+
                 } finally {
                     try {
                         if (conn != null && !conn.isClosed()) {
@@ -210,26 +215,44 @@ public class Main {
                         e.printStackTrace();
                     }
                 }
-            } else if (cmd.equals("article delete")) {
+            } else if (cmd.startsWith("article delete")) {
 
                 Connection conn = null;
                 PreparedStatement pstmt = null;
 
                 try {
-                    // 드라이버 연결
                     Class.forName("org.mariadb.jdbc.Driver");
-                    // 계정 연결
                     String url = "jdbc:mariadb://127.0.0.1:3306/AM_DB_25_03?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
-
                     conn = DriverManager.getConnection(url, "root", "");
-                    System.out.println("연결 성공!");
 
-                    System.out.print("\n삭제할 게시물 번호 : ");
-                    int deleteNum = sc.nextInt();
-                    sc.nextLine();
+                    int deleteId = -1;
 
-                    String sql = "delete FROM article";
-                    sql += " WHERE id = " + deleteNum + ";";
+                    try {
+                        deleteId = Integer.parseInt(cmd.split(" ")[2]);
+                    } catch (Exception e) {
+                        System.out.println("Enter aritlce Number");
+                        continue;
+                    }
+
+                    String sql = "select count(*) from article where id = " + deleteId + ";";
+
+                    pstmt = conn.prepareStatement(sql);
+
+                    ResultSet rs = pstmt.executeQuery();
+
+                    int deleteIdCount = -1;
+
+                    while(rs.next()) {
+                        deleteIdCount = rs.getInt("count(*)");
+                    }
+
+                    if (deleteIdCount == 0) {
+                        System.out.printf("article %d is not exist\n", deleteId);
+                        continue;
+                    }
+
+                    sql = "delete FROM article";
+                    sql += " WHERE id = " + deleteId + ";";
 
                     // sql 명령어를 전달한다.
                     pstmt = conn.prepareStatement(sql);
@@ -239,7 +262,7 @@ public class Main {
                     int affectedrows = pstmt.executeUpdate();
                     System.out.println("rows : " + affectedrows);
 
-                    System.out.printf("[article %d deleted]\n", deleteNum);
+                    System.out.printf("[article %d deleted]\n", deleteId);
 
                 } catch (ClassNotFoundException e) {
                     System.out.println("드라이버 로딩 실패" + e);
