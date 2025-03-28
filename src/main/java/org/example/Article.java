699ca3cca972e4063c437c206bd25df39e65f67a
yunzivv <yunzivv@gmail.com>
2025-03-28 09:53:46 +0900

    AM_DB 세팅

---
+++ src/main/java/org/example/Article.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Article {
+
+    public int id;
+    private String title;
+    private String content;
+
+    Article(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("[Program Started]");
+
+        Scanner sc = new Scanner(System.in);
+        int lastArticleId = 0;
+        List<Article> articles = new ArrayList<>();
+
+        while (true) {
+            System.out.print("\ncmd :");
+            String cmd = sc.nextLine();
+
+            if(cmd.equals("exit")){
+                break;
+            }
+
+            if(cmd.equals("article write")) {
+
+                System.out.println("[write]");
+                int id = ++lastArticleId;
+
+                System.out.print("tile : ");
+                String title = sc.nextLine();
+
+                System.out.print("body : ");
+                String content = sc.nextLine();
+
+                articles.add(new Article(id, title, content));
+                System.out.println("[write success]");
+
+            } else if(cmd.equals("article list")) {
+
+                System.out.println("[list]");
+
+                if(articles.isEmpty()){
+                    System.out.println("[list empty]");
+                    continue;
+                }
+
+                System.out.println(" No |  title ");
+                System.out.println("-------------");
+
+                for(int i = articles.size() - 1; i >= 0; i--){
+                    Article article = articles.get(i);
+                    System.out.printf(" %d  |  %s \n", article.getId(), article.getTitle());
+                }
+
+            }
+        }
+
+        System.out.println("[Program finished]");
+        sc.close();
+    }
+}
